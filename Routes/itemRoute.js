6cb6bdd23e0e7be16a3a66facb1558a2a6e6626b
marinaNeseem <marinaneseem@gmail.com>
2024-04-19 20:24:00 +0200

    New Updats

--- Routes/itemRoute.js
+++ Routes/itemRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const {createItem,getItems,getItem,getItemByName,updateitem,deleteitem,listSavedItems,addToSavedItems} =require('../Controllers/ItemController');
+const {createItem,getItems,getItem,getItemByName,updateitem,deleteitem,listSavedItems,addToSavedItems,search,updateItemStatus } =require('../Controllers/ItemController');
 const router=express.Router();
 router.route('/').get(getItems).post(createItem); 
 router.route('/addtosaveditems/:itemId').post(addToSavedItems);
 router.route("/listSavedItems").get(listSavedItems);
 router.route("/id/:id").put(updateitem).get(getItem).delete(deleteitem);
 router.route("/title/:title").get(getItemByName);
+router.route('/search').get(search);
+router.route('/:itemId/status', ).put(updateItemStatus);
 
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const dotenv =require('dotenv');//for config fil al 7agat aly btt8yer mn eviroment lltanya
 const morgan = require('morgan');//for middleware 34an nn2l al data between req,res
+//for chattt   
+const http=require("http");
+const cors =require("cors");
+
+//Express app
+const app = express();
+
+
+
+var server=http.createServer(app);
+
+var io =require ("socket.io")(server,{
+  cors:
+  {
+    origin:"*"
+  }
+});
+
 
 dotenv.config({path:'config.env'});
 const dbConnection =require('./config/database');
 const categoryRoute=require('./Routes/categoreyRouts');
 const itemRoute= require('./Routes/itemRoute');
 const UserRoute= require('./Routes/userRouts');
-const PersonRoute =require('./Routes/PersonRouts');
+const ChatsRoute = require('./Routes/chatRoute');
+
+const RequestsRoute = require('./Routes/RequestsRoute'); 
+
 
 //creating models in mongo db befor al routes 
 const Cart = require('./modules/CartModel');
@@ -15,7 +36,7 @@ const User = require('./modules/UserModel');
 const Favorite = require('./modules/FavouritesModel');
 const Payment = require('./modules/PaymentModel');
 const Transaction = require('./modules/TransactionModel');
-const Chats = require('./modules/ChatsModel');
+
 
 
 
@@ -25,9 +46,6 @@ const Chats = require('./modules/ChatsModel');
 //call back function to connect database
 dbConnection();
 
-//Express app
-const app = express();
-
 
 
 
@@ -35,6 +53,12 @@ const app = express();
 // Middleware ***al hgat aly al mfrod t7sal between al res,req like parsing to jason
 
  app.use(express.json());//to convert string sended from postman to js.object
+ app.use(cors());
+
+
+ io.on("Connection",(socket)=>{
+  console.log("connected");
+ });
 
 if(process.env.Node_ENV=='development'){
   app.use(morgan('dev'));
@@ -53,10 +77,10 @@ if(process.env.Node_ENV=='development'){
 app.use('/categories',categoryRoute);
 app.use('/item',itemRoute);
 app.use('/User',UserRoute);
-app.use('/person',PersonRoute);
-
+app.use('/Request',RequestsRoute);
+app.use('/chat',ChatsRoute);
 
 const PORT=process.env.PORT||8000;//hy4of al port fe al env file ml2ho4 hya5od al odam al OR
-  app.listen(PORT,()=>{
+  server.listen(PORT,()=>{
     console.log(`app is runing on port ${PORT}`);
   });
