f038a3d6ca4c158c8c108dc7f878a5e59df8b6e6
marinaNeseem <marinaneseem@gmail.com>
2024-06-03 16:35:53 +0300

    M

--- Routes/itemRoute.js
+++ Routes/itemRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const {createItem ,getItems,getItem,getItemByName,updateitem,deleteitem,listSavedItems,addToSavedItems,search 
-    ,RecentItems,GetUserAcceptedItems
+const {createItem ,getItems,getItem,getItemByName,updateitem,deleteitem,listSavedItems,addToSavedItems,SearchItemsByTitle
+    ,RecentItems,GetUserAcceptedItems,filterItemsByCondition,removeFromSavedItems,getExpiredItemsForUser
 } =require('../Controllers/ItemController');
 const router=express.Router();
 router.route('/').get(getItems).post(createItem);
@@ -10,8 +10,9 @@ router.route("/listSavedItems").get(listSavedItems);
 router.route("/RecentPosted").get(RecentItems);
 router.route("/id/:id").put(updateitem).get(getItem).delete(deleteitem);
 router.route("/title/:title").get(getItemByName);
-router.route('/search').get(search);
-
-
+router.route("/SearchItemsByTitle").get(SearchItemsByTitle);
+router.route("/filterItemsByCondition").get(filterItemsByCondition);
+router.route("/removeFromSavedItems/:itemId").delete(removeFromSavedItems);
+router.route("/getExpiredItems").get(getExpiredItemsForUser);
 
 module.exports=router;
